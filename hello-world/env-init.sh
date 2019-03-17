4db99a50d4f11d711277ad09a3389304064a889b
Qi Gu <rgu@Qis-MacBook-Pro.local>
2019-03-17 17:07:21 -0400

    added env

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-# Minikube pre-setup
-minikube config set WantUpdateNotification false
-# TODO: There is a known issue with Katacoda when this is applied. Waiting to move to new version of Minikube.
-# minikube config set bootstrapper kubeadm
-source <(minikube completion bash)
 source <(kubectl completion bash)
 source <(helm completion bash)
 
-clear
+# Helm Setup
+helm init --wait
+helm repo update
 
-echo 'Starting Kubernetes using Minikube...'
-minikube start
+# Setup dashboard via chart like minikube on 30000
+helm install stable/kubernetes-dashboard --name dash --set=service.type=NodePort --set=enableInsecureLogin=true --set=service.nodePort=30000 --set=service.externalPort=80 --namespace kube-system
 
-minikube addons enable dashboard
-kubectl create -f /opt/kubernetes-dashboard.yaml
+{ clear && echo 'Kubernetes with Helm is ready.'; } 2> /dev/null
 
-minikube addons enable heapster
